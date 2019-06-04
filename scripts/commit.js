247ef2b7d695150feac08eb417fbe800d409a074
Faris Mustafa <farismmk@gmail.com>
2019-06-04 03:02:51 -0700

    chore: check argv

--- scripts/commit.js
+++ scripts/commit.js
@@ -1,9 +1,15 @@
-const { exec } = require('shelljs')
+const { execSync } = require('child_process')
 
 console.log(process.npm_config_argv)
 
-exec('exec < /dev/tty && git cz --hook --colors', (code, stdout, stderr) => {
-  console.log('Exit code:', code)
-  console.log('Program output:', stdout)
-  console.log('Program stderr:', stderr)
-})
+execSync('exec < /dev/tty && git cz --hook --colors', { stdio: 'inherit' })
+
+// child_process.execSync('npm init', {stdio: 'inherit'});
+
+// exec('exec < /dev/tty && git cz --hook --colors', (code, stdout, stderr) => {
+//   console.log('Exit code:', code)
+//   console.log('Program output:', stdout)
+//   console.log('Program stderr:', stderr)
+
+//   // process.stdout.write(stdout)
+// })
