9c592318e33b39afc53a4239bf4e5f239d0e51f1
Faris Mustafa <farismmk@gmail.com>
2019-06-04 02:36:45 -0700

    chore: move gitcz into precommit

---
+++ scripts/commit.js
@@ -0,0 +1,7 @@
+const { exec } = require('shelljs')
+console.log(process.env)
+exec('exec < /dev/tty && git cz --hook --colors', (code, stdout, stderr) => {
+  console.log('Exit code:', code)
+  console.log('Program output:', stdout)
+  console.log('Program stderr:', stderr)
+})
